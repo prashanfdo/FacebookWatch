6cb500a9bbd598e21704f3a1464ed2e6130954ad
prashanfdo <prashanfdoo@gmail.com>
2013-11-24 12:03:58 +0530

    initial

---
+++ Extension/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Extension")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Extension")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e770b3c-8450-4a0d-8c89-f03a30a20f41")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Extension/main.js
@@ -0,0 +1,91 @@
+﻿(function () {
+    var elementId = 'fbtimer';
+    var storageSeconds = 'fbwatch_seconds';
+    var storageDay = 'fbwatch_day';
+    //var IDLETO = 1 * 4 * 1000;
+    var idleto = 10 * 1000;
+    var timer = {
+        seconds: 0,
+        pause: true,
+        running: false,
+        reset : function() {
+            this.seconds = 0;
+            putToStorage(storageDay, new Date().toDateString());
+        },
+        start : function() {
+            var savedSeconds = parseInt(getFromStorage(storageSeconds, 0));
+            if (savedSeconds == 0)
+                putToStorage(storageDay, new Date().toDateString());
+            else
+                this.seconds = savedSeconds;
+        },
+        addTick : function() {
+            putToStorage(storageSeconds, ++timer.seconds);
+            if (getFromStorage(storageDay, 0) != new Date().toDateString())
+                timer.reset();
+        }
+    };
+    var updateUi = function () {
+        if (!timer.element) return;
+        var seconds = timer.seconds % 60;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        var minutes = parseInt((timer.seconds % 3600) / 60);
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var hours = parseInt(timer.seconds / 3600);
+        hours = hours > 0 ? hours + ' :' : '';
+        timer.element.innerText = hours + minutes + ' : ' + seconds;
+    };
+    var run = function () {
+        timer.running = true;
+        updateUi();
+        setTimeout(run, 1000);
+        if (timer.pause) return;
+        timer.addTick();
+    }; 
+    var inactiveTo = undefined;
+    var activeListner = function () {
+        timer.pause = false;
+        if (inactiveTo)
+            clearTimeout(inactiveTo);
+        inactiveTo = setTimeout(function () {
+            timer.pause = true;
+        }, idleto);
+    };
+    window.addEventListener('mousemove', activeListner);
+    window.addEventListener('click', activeListner);
+    window.addEventListener('keypress', activeListner);
+    window.addEventListener('keydown', activeListner);
+    window.addEventListener('focus', function () {
+        timer.pause = false;
+        timer.start();
+    });
+    window.addEventListener('blur', function () {
+        timer.pause = true;
+    });
+    var init = function () {
+        if (!timer.running) {
+            timer.start();
+            run();
+        }
+        var ul = document.getElementById('pageNav');
+        if (ul && !timer.element) {
+            var li = document.createElement('li');
+            li.innerHTML = '<span title="Double click to reset" class="navLink bigPadding" id="' + elementId + '"></span>';
+            li.className = 'navItem middleItem';
+            ul.insertBefore(li, ul.firstChild);
+            timer.element = document.getElementById(elementId);
+            timer.element.addEventListener('dblclick', function() {
+                timer.reset();
+            });
+        }
+    };
+    var putToStorage = function (key, object) {
+        localStorage.setItem(key, object);
+    };
+    var getFromStorage = function (key, def) { 
+        return localStorage.getItem(key) || def || 0;
+    };
+    //window.addEventListener('load', function () {
+    init();
+    //});
+})();
